19642335769e9cffd6e61227030ef8e8862a37e4
unknown <avaneesh.ponna99@gmail.com>
2020-08-18 10:49:34 +0530

    Second Commit

--- apis/adminApi.js
+++ apis/adminApi.js
@@ -1,6 +1,7 @@
+//bharadwaj
+
 const exp = require("express");
 const router = exp.Router();
-
 
 // router.get("/:id", (req, res, next) => {
 //   if (dbo === "undefined") {
@@ -22,15 +23,14 @@ const router = exp.Router();
 // });
 
 router.get("/viewbookings", (req, res, next) => {
-    req.app.locals.bookingsCollectionObj.find()
-    .toArray((err, result) => {
-      if (err) {
-        console.log("some error ocuured while getting bookings");
-        return res.status(401).json({ messsage: "Bookins cant be found" });
-      } else {
-        res.status(200).json({ message: "success", bookings: result });
-      }
-    });
+  req.app.locals.bookingsCollectionObj.find().toArray((err, result) => {
+    if (err) {
+      console.log("some error ocuured while getting bookings");
+      return res.status(401).json({ messsage: "Bookins cant be found" });
+    } else {
+      res.status(200).json({ message: "success", bookings: result });
+    }
+  });
 });
 
 module.exports = router;
